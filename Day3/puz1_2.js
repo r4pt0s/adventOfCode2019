ab3e6d88327a04a5728dc32c582ae5a6c72e470c
r4pt0s <r4pt0s@protonmail.com>
2019-12-04 15:46:37 +0100

    refactor solution because of bad performance. Replaced Arrays with Hash table

--- Day3/puz1_2.js
+++ Day3/puz1_2.js
@@ -1,26 +1,27 @@
 const { input } = require("./shared");
 
 const testInput = input.split("\n");
-/* const testInput = `R75,D30,R83,U83,L12,D49,R71,U7,L72
-U62,R66,U55,R34,D71,R55,D58,R83`.split("\n"); */
 
 const manhattenDistance = (x1, x2, y1, y2) =>
   Math.abs(x1 - x2) + Math.abs(y1 - y2);
 
 const centralPort = { x: 0, y: 0 };
 
+const totalSteps = {
+  w1: 0,
+  w2: 0
+};
+
 const wires = {
   w1: {
     x: centralPort.x,
     y: centralPort.y,
-    coordinates: [],
-    totalSteps: []
+    coordinates: {}
   },
   w2: {
     x: centralPort.x,
     y: centralPort.y,
-    coordinates: [],
-    totalSteps: []
+    coordinates: {}
   },
   intersections: []
 };
@@ -34,144 +35,62 @@ const extractDirectionFromSteps = instruction => {
   };
 };
 
+const storeWayPoints = (steps, wire, axis, direction) => {
+  const { x, y } = wires[wire];
+
+  for (let i = 0; i < steps; i++) {
+    const key =
+      axis === "y" ? `${x};${y + i * direction}` : `${x + i * direction};${y}`;
+
+    totalSteps[wire] += 1;
+
+    wires[wire].coordinates[key] = totalSteps[wire];
+    if (wire === "w2" && wires.w1.coordinates[key]) {
+      const dynamicIntersectionPoints = {
+        x: axis === "x" ? x + i * direction : x,
+        y: axis === "y" ? y + i * direction : y
+      };
+
+      wires.intersections.push({
+        point: `${key}`,
+        distance: manhattenDistance(
+          dynamicIntersectionPoints.x,
+          centralPort.x,
+          dynamicIntersectionPoints.y,
+          centralPort.y
+        ),
+        stepsToIntersection:
+          wires.w1.coordinates[key] + wires.w2.coordinates[key] - 2
+      });
+    }
+  }
+};
+
 const lockPoints = (instructionSet, wire) => {
   instructionSet.forEach(instruction => {
     const { direction, steps } = extractDirectionFromSteps(instruction);
 
-    console.log("calc");
-
     switch (direction) {
       case "U":
-        for (let i = 0; i < steps; i++) {
-          wires[wire].coordinates.push(`${wires[wire].x};${wires[wire].y - i}`);
-          if (wire === "w2") {
-            if (
-              wires.w1.coordinates.includes(
-                `${wires[wire].x};${wires[wire].y - i}`
-              )
-            ) {
-              wires.intersections.push({
-                point: `${wires[wire].x};${wires[wire].y - i}`,
-                distance: manhattenDistance(
-                  wires[wire].x,
-                  centralPort.x,
-                  wires[wire].y - i,
-                  centralPort.y
-                ),
-                indexStore: {
-                  w1: wires.w1.coordinates.indexOf(
-                    `${wires[wire].x};${wires[wire].y - i}`
-                  ),
-                  w2: wires.w2.coordinates.indexOf(
-                    `${wires[wire].x};${wires[wire].y - i}`
-                  )
-                }
-              });
-            }
-          }
-        }
+        storeWayPoints(steps, wire, "y", -1);
         wires[wire].y -= steps;
         break;
       case "D":
-        for (let i = 0; i < steps; i++) {
-          wires[wire].coordinates.push(`${wires[wire].x};${wires[wire].y + i}`);
-          if (wire === "w2") {
-            if (
-              wires.w1.coordinates.includes(
-                `${wires[wire].x};${wires[wire].y + i}`
-              )
-            ) {
-              wires.intersections.push({
-                point: `${wires[wire].x};${wires[wire].y + i}`,
-                distance: manhattenDistance(
-                  wires[wire].x,
-                  centralPort.x,
-                  wires[wire].y + i,
-                  centralPort.y
-                ),
-                indexStore: {
-                  w1: wires.w1.coordinates.indexOf(
-                    `${wires[wire].x};${wires[wire].y + i}`
-                  ),
-                  w2: wires.w2.coordinates.indexOf(
-                    `${wires[wire].x};${wires[wire].y + i}`
-                  )
-                }
-              });
-            }
-          }
-        }
+        storeWayPoints(steps, wire, "y", 1);
         wires[wire].y += steps;
         break;
       case "L":
-        for (let i = 0; i < steps; i++) {
-          wires[wire].coordinates.push(`${wires[wire].x - i};${wires[wire].y}`);
-          if (wire === "w2") {
-            if (
-              wires.w1.coordinates.includes(
-                `${wires[wire].x - i};${wires[wire].y}`
-              )
-            ) {
-              wires.intersections.push({
-                point: `${wires[wire].x - i};${wires[wire].y}`,
-                distance: manhattenDistance(
-                  wires[wire].x - i,
-                  centralPort.x,
-                  wires[wire].y,
-                  centralPort.y
-                ),
-                indexStore: {
-                  w1: wires.w1.coordinates.indexOf(
-                    `${wires[wire].x - i};${wires[wire].y}`
-                  ),
-                  w2: wires.w2.coordinates.indexOf(
-                    `${wires[wire].x - i};${wires[wire].y}`
-                  )
-                }
-              });
-            }
-          }
-        }
+        storeWayPoints(steps, wire, "x", -1);
         wires[wire].x -= steps;
         break;
       case "R":
-        for (let i = 0; i < steps; i++) {
-          wires[wire].coordinates.push(`${wires[wire].x + i};${wires[wire].y}`);
-          if (wire === "w2") {
-            if (
-              wires.w1.coordinates.includes(
-                `${wires[wire].x + i};${wires[wire].y}`
-              )
-            ) {
-              wires.intersections.push({
-                point: `${wires[wire].x - +i};${wires[wire].y}`,
-                distance: manhattenDistance(
-                  wires[wire].x + i,
-                  centralPort.x,
-                  wires[wire].y,
-                  centralPort.y
-                ),
-                indexStore: {
-                  w1: wires.w1.coordinates.indexOf(
-                    `${wires[wire].x + i};${wires[wire].y}`
-                  ),
-                  w2: wires.w2.coordinates.indexOf(
-                    `${wires[wire].x + i};${wires[wire].y}`
-                  )
-                }
-              });
-            }
-          }
-        }
+        storeWayPoints(steps, wire, "x", 1);
         wires[wire].x += steps;
         break;
       default:
         break;
     }
-    wires[wire].totalSteps.push(steps);
   });
-
-  console.log(wires[wire].coordinates);
 };
 
 lockPoints(testInput[0].split(","), "w1");
@@ -193,8 +112,6 @@ console.log(
     Math,
     wires.intersections
       .slice(1)
-      .map(
-        intersection => intersection.indexStore.w1 + intersection.indexStore.w2
-      )
+      .map(intersection => intersection.stepsToIntersection)
   )
 );
