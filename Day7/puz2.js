70c747fea020849e91574f4bc314225f8fdea67a
r4pt0s <r4pt0s@protonmail.com>
2019-12-09 16:43:16 +0100

    ongoing work of Day7 part 2

--- Day7/puz2.js
+++ Day7/puz2.js
@@ -1,8 +1,9 @@
 const { softwareController } = require("./shared");
 
 let myInput = null;
-
 let outPutByOpCode4 = [0];
+
+process.stdout.write("\033c");
 
 const extractParameterModeParts = instruction => {
   const arrayVersion = instruction.toString().split("");
@@ -13,17 +14,31 @@ const extractParameterModeParts = instruction => {
     a: Number(arrayVersion.pop()) || 0
   };
 
-  console.log(slots);
+  //console.log(slots);
 
   return slots;
 };
 
-const program = (phaseInput) => (opcode, instructions, a, b, c, controlSoftware) => {
-  const [valueAtPosOne = 0, valueAtPosTwo = 0, replaceValue = 0] = instructions;
-  const modeP1 = c === 0 ? Number(controlSoftware[valueAtPosOne]) : Number(valueAtPosOne);
-  const modeP2 = b === 0 ? Number(controlSoftware[valueAtPosTwo]) : Number(valueAtPosTwo);
-  const returnObject = { method: "software", value: false, controlSoftware: null };
+let phaseOutput = null;
 
+const program = phaseInput => (
+  opcode,
+  instructions,
+  a,
+  b,
+  c,
+  controlSoftware
+) => {
+  const [valueAtPosOne = 0, valueAtPosTwo = 0, replaceValue = 0] = instructions;
+  const modeP1 =
+    c === 0 ? Number(controlSoftware[valueAtPosOne]) : Number(valueAtPosOne);
+  const modeP2 =
+    b === 0 ? Number(controlSoftware[valueAtPosTwo]) : Number(valueAtPosTwo);
+  const returnObject = {
+    method: "software",
+    value: false,
+    controlSoftware: null
+  };
 
   switch (opcode) {
     case 1:
@@ -33,22 +48,27 @@ const program = (phaseInput) => (opcode, instructions, a, b, c, controlSoftware)
       controlSoftware[replaceValue] = modeP1 * modeP2;
       break;
     case 3:
-      if (phaseInput.length > 1)
+      /*  if (phaseInput.length > 1) {
         controlSoftware[valueAtPosOne] = phaseInput.shift();
-      else
+      } else {
         controlSoftware[valueAtPosOne] = phaseInput[0];
+      } */
+      controlSoftware[valueAtPosOne] = phaseInput;
+
+      console.log(phaseInput);
       break;
     case 4:
-      returnObject.method = 'output';
+      returnObject.method = "output";
       returnObject.value = controlSoftware[valueAtPosOne];
+      phaseOutput = controlSoftware[valueAtPosOne];
       break;
 
     case 5:
-      returnObject.method = 'jump';
+      returnObject.method = "jump";
       returnObject.value = modeP1 > 0 ? modeP2 : false;
       break;
     case 6:
-      returnObject.method = 'jump';
+      returnObject.method = "jump";
       returnObject.value = modeP1 === 0 ? modeP2 : false;
       break;
     case 7:
@@ -58,6 +78,7 @@ const program = (phaseInput) => (opcode, instructions, a, b, c, controlSoftware)
       controlSoftware[replaceValue] = modeP1 === modeP2 ? 1 : 0;
       break;
     case 99:
+      console.log("PHASEOUTPUT: ", phaseOutput);
       returnObject.method = "halt";
       returnObject.value = controlSoftware[0];
       break;
@@ -68,15 +89,17 @@ const program = (phaseInput) => (opcode, instructions, a, b, c, controlSoftware)
 
   returnObject.controlSoftware = controlSoftware;
 
-  //console.log(controlSoftware);
-
   return returnObject;
 };
 
-const intCodeComputer = (programWithInput, controlSoftware) => {
+const intCodeComputer = (programWithInput, controlSoftware, input) => {
   let instructionPointer = 0;
   let halt = false;
   const iterationCount = controlSoftware.length;
+  outPutByOpCode4 = [];
+  let returnObject = null;
+
+  //let input = null;
 
   const opCodeParamsCount = {
     opcode0: 3,
@@ -88,7 +111,7 @@ const intCodeComputer = (programWithInput, controlSoftware) => {
     opcode6: 2,
     opcode7: 3,
     opcode8: 3,
-    opCode99: 0
+    opCode99: 1
   };
 
   while (instructionPointer < iterationCount && !halt) {
@@ -117,12 +140,14 @@ const intCodeComputer = (programWithInput, controlSoftware) => {
       opCodeParamsCount[`opcode${opCode}`],
       "instructions: ",
       instructions,
+      "\n",
+      controlSoftware.join(","),
       "\n"
     );
 
     returnObject = programWithInput(
-      opCode, instructions
-      ,
+      opCode,
+      instructions,
       a,
       b,
       c,
@@ -130,19 +155,23 @@ const intCodeComputer = (programWithInput, controlSoftware) => {
     );
 
     switch (returnObject.method) {
-      case 'jump':
+      case "jump":
         if (returnObject.value !== false) {
           instructionPointer = returnObject.value;
         } else {
           instructionPointer += opCodeParamsCount[`opcode${opCode}`] + 1;
         }
         break;
-      case 'output':
-        outPutByOpCode4.push(returnObject.value)
+      case "output":
+        console.log("output ", returnObject.value);
+        controlSoftware = returnObject.controlSoftware;
         instructionPointer += opCodeParamsCount[`opcode${opCode}`] + 1;
+        programWithInput = program(returnObject.value);
+        //input = returnObject.value;
         break;
-      case 'halt':
-        //return returnObject.value;
+      //return controlSoftware;
+      case "halt":
+        console.log("HALT");
         halt = true;
       default:
         instructionPointer += opCodeParamsCount[`opcode${opCode}`] + 1;
@@ -152,30 +181,8 @@ const intCodeComputer = (programWithInput, controlSoftware) => {
     controlSoftware = returnObject.controlSoftware;
   }
 
-  return controlSoftware;
-
+  return { controlSoftware, returnObject };
 };
-
-
-//Thanks to stackovewflow => https://stackoverflow.com/a/49652789
-function arrayCreate(array, size) {
-  var result = [];
-  array.forEach(function iter(parts) {
-    return function (v) {
-      var temp = parts.concat(v);
-      if (parts.includes(v)) {
-        return;
-      }
-      if (temp.length === size) {
-        result.push(temp);
-        return;
-      }
-      array.forEach(iter(temp));
-    }
-  }([]));
-  return result;
-}
-
 
 const possibleEvaluatedPhases = arrayCreate([5, 6, 7, 8, 9], 5);
 const signals = [];
@@ -185,18 +192,47 @@ let amp3 = null;
 let amp4 = null;
 let amp5 = 0;
 
-amp5 = '3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5'.split(',').map(Number);
+amp5 = {
+  controlSoftware: "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"
+    .split(",")
+    .map(Number)
+};
+
+const rounds = amp5.length;
+
 const sequenz = [9, 8, 7, 6, 5];
 
-while (outPutByOpCode4[outPutByOpCode4.length - 1] !== NaN) {
-  amp1 = intCodeComputer(program([sequenz[0], outPutByOpCode4.pop()]), amp5);
-  amp2 = intCodeComputer(program([sequenz[1], outPutByOpCode4.pop()]), amp1);
-  amp3 = intCodeComputer(program([sequenz[2], outPutByOpCode4.pop()]), amp2);
-  amp4 = intCodeComputer(program([sequenz[3], outPutByOpCode4.pop()]), amp3);
-  amp5 = intCodeComputer(program([sequenz[4], outPutByOpCode4.pop()]), amp4);
-}
+//possibleEvaluatedPhases.forEach((sequenz, j) => {
+outPutByOpCode4 = [0];
+let i = 0;
+//18216;
 
-console.log(outPutByOpCode4);
+//while (i < 125) {
+process.stdout.write("\033c");
+// Sequenze ${j} of ${possibleEvaluatedPhases.length}
+console.log(`
+   
+    Processed round ${i} of ${rounds}
+    Current signals: ${outPutByOpCode4}`);
+
+//amp should wait until it receives it next input
+// think of generator functions for possible solutions
+//
+
+amp1 = intCodeComputer(program(sequenz[0]), amp5.controlSoftware);
+amp2 = intCodeComputer(program(sequenz[1]), amp1.controlSoftware);
+amp3 = intCodeComputer(program(sequenz[2]), amp2.controlSoftware);
+amp4 = intCodeComputer(program(sequenz[3]), amp3.controlSoftware);
+amp5 = intCodeComputer(program(sequenz[4]), amp4.controlSoftware);
+
+i++;
+
+//}
+
+//signals.push(outPutByOpCode4[outPutByOpCode4.length - 1]);
+//});
+
+console.log(phaseOutput);
 
 /* possibleEvaluatedPhases.forEach((phases, i) => {
 
@@ -216,3 +252,23 @@ console.log(outPutByOpCode4);
 //139629729
 //console.log(signals);
 
+//Thanks to stackovewflow => https://stackoverflow.com/a/49652789
+function arrayCreate(array, size) {
+  var result = [];
+  array.forEach(
+    (function iter(parts) {
+      return function(v) {
+        var temp = parts.concat(v);
+        if (parts.includes(v)) {
+          return;
+        }
+        if (temp.length === size) {
+          result.push(temp);
+          return;
+        }
+        array.forEach(iter(temp));
+      };
+    })([])
+  );
+  return result;
+}
